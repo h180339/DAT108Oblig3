a01046a6b77ac0a0dd9d6666777fc7453078aa44
h180339 <h@g.gov>
2018-11-01 09:31:51 +0100

    Fikset greier med tomme felter i logginn

--- src/no/hvl/dat108/Paamelding.java
+++ src/no/hvl/dat108/Paamelding.java
@@ -50,7 +50,6 @@ public class Paamelding extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/Paamelding.jsp")
-                .forward(request, response);
+        request.getRequestDispatcher("WEB-INF/Paamelding.jsp").forward(request, response);
     }
 }

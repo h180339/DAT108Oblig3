ea357f2c20b2e6bf9e393529c03b0abf0823870b
182714 <adrian.holthe@live.no>
2018-11-01 15:24:08 +0100

    SQL Database, works stores and get data, go nuts

---
+++ database.sql
@@ -0,0 +1,19 @@
+
+set search_path = dat108oblig3;
+
+CREATE TABLE bruker
+(
+   fornavn          VARCHAR (100),
+   etternavn        VARCHAR (100),
+   mobil            VARCHAR (60) UNIQUE,
+   passwordHash     VARCHAR (100),
+   kjonn            VARCHAR (40),
+   PRIMARY KEY (mobil),
+);
+
+
+
+
+
+
+

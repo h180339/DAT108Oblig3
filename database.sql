ecab7c87ad7127c4ed079d32e2321d89a352145d
182714 <adrian.holthe@live.no>
2018-11-02 10:32:49 +0100

    SQL Database, works stores and get data, go nuts

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ CREATE TABLE bruker
 (
    fornavn          VARCHAR (100),
    etternavn        VARCHAR (100),
-   mobil            VARCHAR (60),
+   mobil            VARCHAR (60) UNIQUE,
    passordHash     VARCHAR (100),
    kjonn            VARCHAR (40),
    PRIMARY KEY (mobil)
